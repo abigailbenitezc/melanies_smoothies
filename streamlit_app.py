fa7bb191c4698f1b4ee8ffa719c52f827ecb9f99
abigailbenitezc <abigailbenitezc@gmail.com>
2025-08-13 14:20:27 -0600

    Update streamlit_app.py
    impoty the "request" library

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,7 @@ if ingridients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-        
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
